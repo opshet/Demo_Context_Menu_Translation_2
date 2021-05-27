d96d461517a13eb3bffa3e2f7597e3fbfe6b3b78
opshet <20010021@myrp.edu.sg>
2021-05-27 12:27:50 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20010021/demo_context_menu_translation_2/MainActivity.java
@@ -0,0 +1,57 @@
+package sg.edu.rp.c346.id20010021.demo_context_menu_translation_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvTranslatedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTranslatedText=findViewById(R.id.tvTranslatedText);
+
+        registerForContextMenu(tvTranslatedText);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        //…
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        // …
+        if (item.getItemId() == R.id.EnglishSelection) { //check whether the selected menu item ID is 0
+            //code for action
+            Toast.makeText(MainActivity.this, "English is chosen", Toast.LENGTH_SHORT).show();
+            tvTranslatedText.setText("Hello");
+            return true; //menu item successfully handled
+        } else if (item.getItemId() == R.id.italianSelection) { //check if the selected menu item ID is 1
+            //code for action
+            Toast.makeText(MainActivity.this, "Italian is chosen", Toast.LENGTH_SHORT).show();
+            tvTranslatedText.setText("Ciao");
+            return true;  //menu item successfully handled
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+
+
+
+}
